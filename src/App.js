974da294dc0b92322a40e3e059370b40e937520e
Gary Anikin <garyanikin@gmail.com>
2021-05-30 15:43:11 +0300

    Init project, add favicon, setup navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <NavBar />
     </div>
   );
 }
+
+const NavBar = () => {
+  const menus = [
+    { icon: "", caption: "Home", link: "#home" },
+    { icon: "", caption: "Connect", link: "#connect" },
+    { icon: "", caption: "Calculator", link: "calculator" },
+    { icon: "", caption: "Community", link: "community" },
+  ];
+  const activeLink = "#home";
+
+  return (
+    <nav
+      class="navbar fixed-bottom navbar-expand bg-white"
+      style={{
+        boxShadow: "0px -4px 16px rgba(21, 48, 49, 0.08)",
+      }}
+    >
+      <div class="container-fluid">
+        <ul class="navbar-nav" style={{ width: "100%" }}>
+          {menus.map(({ icon, caption, link }) => (
+            <li class="nav-item text-center" style={{ flex: "1 1" }}>
+              <a
+                class={`nav-link ${link === activeLink ? "active" : ""}`}
+                aria-current={caption}
+                href={link}
+              >
+                <div class="col-12">
+                  <div className="navbar-icon">{icon}</div>
+                </div>
+                <div class="col-12 caption">{caption}</div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default App;
